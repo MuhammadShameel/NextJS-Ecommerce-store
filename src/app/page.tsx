13ec59289ace22f3966173d8fbecb298e29d2055
MuhammadShameel <mshameelkz@gmail.com>
2024-05-01 15:12:12 +0500

    add product components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Nav from "./components/Nav";
+import Product from "./components/Product";
+import "../app/globals.css";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Nav />
-    </>
+      <Product />
+    </div>
   );
 }
