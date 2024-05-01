0a0cc8da55918824ee696060b2320e85b1c4588b
MuhammadShameel <mshameelkz@gmail.com>
2024-05-01 15:12:44 +0500

    product section heading has been placeed

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="p-6">
+        <h2 className="text-center">Or subscribe to the newsletter</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
