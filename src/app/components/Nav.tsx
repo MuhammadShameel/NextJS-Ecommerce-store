ea5f69022b5b3166217b3151cf4058542fb86140
MuhammadShameel <mshameelkz@gmail.com>
2024-05-01 14:00:52 +0500

    created bottom part of navbar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -8,9 +8,9 @@ import { BiSolidShoppingBag } from "react-icons/bi";
 const Nav = () => {
   return (
     <div className="container mx-auto">
-      <nav className="flex items-center justify-between flex-wrap bg-white p-6 border-b-2 border-gray-50">
+      <nav className="flex items-center justify-between flex-wrap bg-white p-6 ">
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <button className="flex items-center px-3 py-2   text-primary  hover:text-gray-500 ">
+          <button className="flex items-center py-2   text-primary  hover:text-gray-500 transition-colors duration-300 ease-in-out">
             <FiSearch className="text-xl" />
           </button>
           <div className="flex items-center flex-shrink-0 text-white mx-auto">
@@ -19,7 +19,7 @@ const Nav = () => {
           <div className="text-sm  text-right">
             <a
               href="#account"
-              className="block mt-4 lg:inline-block lg:mt-0 text-primary hover:text-gray-500 mr-4"
+              className="block mt-4 lg:inline-block lg:mt-0 text-primary hover:text-gray-500 mr-4 transition-colors duration-300 ease-in-out"
             >
               <span className="flex">
                 {" "}
@@ -28,13 +28,60 @@ const Nav = () => {
             </a>
             <a
               href="#shopping"
-              className="block mt-4 lg:inline-block lg:mt-0 text-primary hover:text-gray-500"
+              className="block mt-4 lg:inline-block lg:mt-0 text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
             >
               <span className="flex">
                 <BiSolidShoppingBag className="text-xl mr-1" /> Shopping
               </span>
             </a>
           </div>
+        </div>
+      </nav>
+      <hr className="border-t-2 border-gray-50" />
+      <nav className="flex justify-center bg-white p-6">
+        <div className="flex justify-between w-full  mx-auto">
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Jewelry & Accessories
+          </a>
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Clothing & Shoes
+          </a>
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Home & Living
+          </a>
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Wedding & Party
+          </a>
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Toys & Entertainment
+          </a>
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Art & Collectibles
+          </a>
+          <a
+            href="#shopping"
+            className="text-primary hover:text-gray-500 transition-colors duration-300 ease-in-out"
+          >
+            Craft Supplies & Tools
+          </a>
         </div>
       </nav>
     </div>
