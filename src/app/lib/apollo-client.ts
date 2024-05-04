bd14c210d90b496f3c1476182c4acf33464dd52c
MuhammadShameel <mshameelkz@gmail.com>
2024-05-04 22:56:10 +0500

    add authentic url

---
+++ src/app/lib/apollo-client.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// const client = new ApolloClient({
+//   uri: "https://flyby-router-demo.herokuapp.com/",
+//   cache: new InMemoryCache(),
+// });
+
+const client = new ApolloClient({
+  uri: "https://explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
