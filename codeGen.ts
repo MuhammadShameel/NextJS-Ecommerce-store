d18342ef090ec07a671aa116259169cafd81d5af
MuhammadShameel <mshameelkz@gmail.com>
2024-05-10 20:56:29 +0500

    create code gen file

---
+++ codeGen.ts
@@ -0,0 +1,19 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema:
+    "https://explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com/graphql",
+  documents: ["src/**/*.{ts,tsx}"],
+  generates: {
+    "./src": {
+      preset: "client",
+      plugins: [],
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
