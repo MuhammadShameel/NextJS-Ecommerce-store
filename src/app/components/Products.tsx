eb95ecedb4abf678acd813cda05cbcd675d31508
MuhammadShameel <mshameelkz@gmail.com>
2024-05-13 22:24:33 +0500

    edit product section

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -1,49 +1,46 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import SkeletonLoader from "./SkeletonLoader";
+import AllProducts from "./AllProducts";
 
-import jeansProduct from "../../../public/images/joggers.png";
 import food from "../../../public/images/food-card.jpg";
 import Image from "next/image";
 
 import { MdFilterAlt } from "react-icons/md";
 
-const Products = () => {
-  const [tagId, setTagId] = useState("");
-
-  const GET_DATA = gql`
-    query {
-      tags(shopId: "cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==") {
-        nodes {
-          _id
-          name
-          displayTitle
-          slug
-        }
+const GET_DATA = gql`
+  query ($tagIds: [ID!]) {
+    tags(shopId: "cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==") {
+      nodes {
+        _id
+        name
+        displayTitle
+        slug
       }
-      catalogItems(
-        shopIds: ["cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ=="]
-        tagIds: ["${tagId}"]
-      ) {
-        edges {
-          node {
-            ... on CatalogItemProduct {
-              product {
-                title
-                description
-                pricing {
-                  displayPrice
-                }
+    }
+    catalogItems(
+      shopIds: ["cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ=="]
+      tagIds: $tagIds
+    ) {
+      edges {
+        node {
+          ... on CatalogItemProduct {
+            product {
+              title
+              description
+              pricing {
+                displayPrice
+              }
+              _id
+              variants {
                 _id
-                variants {
-                  _id
-                  title
-                  media {
-                    URLs {
-                      small
-                    }
+                title
+                media {
+                  URLs {
+                    small
                   }
                 }
               }
@@ -52,41 +49,31 @@ const Products = () => {
         }
       }
     }
-  `;
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  }
+`;
+const Products = () => {
+  const [tagId, setTagId] = useState("");
+
   const { loading, error, data } = useQuery(GET_DATA, {
     variables: {
-      shopIds: ["cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ=="],
+      tagIds: tagId ? [tagId] : null,
     },
   });
+  const [showAllProducts, setShowAllProducts] = useState(false);
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const queryParameters = useSearchParams();
   const search = queryParameters.get("tag");
-  useEffect(() => {
-    console.log("xyz");
-  }, []);
+
+  const handleAllProductsClick = () => {
+    setTagId("");
+    setShowAllProducts(false);
+  };
 
   if (loading)
     return (
-      <div className="flex items-center justify-center container mx-auto border border-gray-200 rounded-lg bg-gray-50 h-40">
-        <div role="status">
-          <svg
-            aria-hidden="true"
-            className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
+      <div>
+        <SkeletonLoader />
       </div>
     );
   if (error)
@@ -139,7 +126,7 @@ const Products = () => {
                   <ul className="absolute z-10 top-full rounded-lg mt-4 left-0 w-[140px] bg-white text-gray-500 shadow-lg py-1">
                     <li>
                       <Link
-                        href="#tshirt"
+                        href="/"
                         className="text-gray-500 hover:text-primary transition-colors duration-300 ease-in-out ms-2 mt-2"
                       >
                         All Products
@@ -164,8 +151,9 @@ const Products = () => {
                   <Link
                     href="/"
                     className={`${
-                      !search ? "font-bold text-black" : ""
+                      !search ? " text-red-500" : ""
                     } text-gray-500 hover:text-primary transition-colors duration-300 ease-in-out`}
+                    onClick={handleAllProductsClick}
                     scroll={false}
                   >
                     All Products
@@ -180,11 +168,12 @@ const Products = () => {
                           query: { tag: menuItems.slug },
                         }}
                         className={`${
-                          isActive ? "font-bold text-black" : ""
+                          isActive ? " text-red-500" : ""
                         } text-gray-500  transition-colors duration-300 ease-in-out`}
                         onClick={() => {
                           setTagId(menuItems._id);
                         }}
+                        scroll={false}
                       >
                         {menuItems.displayTitle}
                       </Link>
@@ -198,71 +187,54 @@ const Products = () => {
               Filter
             </button>
           </nav>
+
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            {data.catalogItems.edges.map((edge: any) => {
-              const { node } = edge;
-              const { product } = node;
-              return (
-                <div
-                  key={product._id}
-                  className="product-card relative flex flex-col overflow-hidden bg-white hover:shadow-md transition"
-                >
-                  <Link className="relative flex overflow-hidden" href="#">
-                    <Image
-                      className="object-cover sm:mx-auto"
-                      src={food}
-                      alt="product image"
-                    />
-                  </Link>
-                  <div className="mt-4 px-3 pb-5">
+            {showAllProducts ? (
+              <AllProducts />
+            ) : (
+              data.catalogItems.edges.map((edge: { node: any }) => {
+                const { node } = edge;
+                const { product } = node;
+                return (
+                  <div
+                    key={product._id}
+                    className="product-card relative flex flex-col overflow-hidden bg-white hover:shadow-md transition"
+                  >
                     <Link href="#">
-                      <h5 className="text-sm tracking-tight">
-                        {product.title}
-                      </h5>
+                      <Image
+                        className="object-cover sm:mx-auto"
+                        src={food}
+                        alt="product image"
+                      />
                     </Link>
-                    <div className="mt-2 flex items-center justify-between">
-                      <h5 className="text-sm tracking-tight text-gray-400">
-                        {product.description}
-                      </h5>
-                    </div>
-                    <div className="mt-2 flex justify-between">
-                      {product.variants.map((variant: any) => (
-                        <p key={variant._id} className="text-xs text-gray-500">
-                          {variant.title}
-                        </p>
-                      ))}
-                      {product.pricing.map((price: any, index: number) => (
-                        <h5 key={index} className="text-sm tracking-tight">
-                          {price.displayPrice}
+                    <div className="mt-4 px-3 pb-5">
+                      <Link href="#">
+                        <h5 className="text-sm tracking-tight">
+                          {product.title}
                         </h5>
-                      ))}
+                      </Link>
+                      <div className="mt-2 flex justify-between">
+                        {product.pricing.map(
+                          (
+                            price: {
+                              displayPrice: any;
+                            },
+                            index: React.Key | null | undefined
+                          ) => (
+                            <h5
+                              key={index}
+                              className="text-sm ms-auto tracking-tight"
+                            >
+                              {price.displayPrice}
+                            </h5>
+                          )
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-            <div className="product-card relative flex flex-col overflow-hidden bg-white hover:shadow-md transition">
-              <Link className="relative flex overflow-hidden " href="#">
-                <Image
-                  className="object-cover sm:mx-auto"
-                  src={jeansProduct}
-                  alt="product image"
-                />
-              </Link>
-              <div className="mt-4 px-3 pb-5">
-                <Link href="#">
-                  <h5 className="text-sm tracking-tight ">
-                    Adicolor Classics Joggers
-                  </h5>
-                </Link>
-                <div className="mt-2  flex items-center justify-between">
-                  <h5 className="text-sm tracking-tight text-gray-400">
-                    Dress
-                  </h5>
-                  <h5 className="text-sm tracking-tight ">$63.85</h5>
-                </div>
-              </div>
-            </div>
+                );
+              })
+            )}
           </div>
         </div>
       </div>
