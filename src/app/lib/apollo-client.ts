5e9503bfb6e165a1b1ac0f4b1e3a65ee7326939d
MuhammadShameel <mshameelkz@gmail.com>
2024-05-13 22:24:19 +0500

    remove unnecessary comment

--- src/app/lib/apollo-client.ts
+++ src/app/lib/apollo-client.ts
@@ -1,10 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-// const client = new ApolloClient({
-//   uri: "https://flyby-router-demo.herokuapp.com/",
-//   cache: new InMemoryCache(),
-// });
-
 const client = new ApolloClient({
   uri: "https://explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com/graphql",
   cache: new InMemoryCache(),
