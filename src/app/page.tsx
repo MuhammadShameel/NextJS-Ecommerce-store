f7405a187f409f2244081d0b7c6866d08b38dfd8
MuhammadShameel <mshameelkz@gmail.com>
2024-05-14 16:53:32 +0500

    remove nav and footer from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,15 @@
 
 import Nav from "./components/Nav";
 import Product from "./components/Products";
-import Footer from "./components/Footer";
 import "../app/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "./lib/apollo-client";
-import SkeletonLoader from "./components/SkeletonLoader";
 
 export default function Home() {
   return (
     <ApolloProvider client={client}>
       <div>
-        <Nav />
         <Product />
-        <Footer />
       </div>
     </ApolloProvider>
   );
