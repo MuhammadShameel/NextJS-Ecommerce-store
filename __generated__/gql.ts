006f7b6ddcb0e28ac93febdfe347d253e01d863d
MuhammadShameel <mshameelkz@gmail.com>
2024-05-23 20:50:52 +0500

    add gql file

--- __generated__/gql.ts
+++ __generated__/gql.ts
@@ -2,7 +2,22 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
-const documents = [];
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ */
+const documents = {
+    "\n  query catalogItemProduct($shopId: ID!, $slugOrId: String!) {\n    catalogItemProduct(shopId: $shopId, slugOrId: $slugOrId) {\n      product {\n        title\n        description\n        variants {\n          _id\n          title\n          attributeLabel\n          optionTitle\n          pricing {\n            displayPrice\n            price\n            currency {\n              code\n            }\n          }\n        }\n        primaryImage {\n          URLs {\n            medium\n            original\n          }\n        }\n      }\n    }\n  }\n": types.CatalogItemProductDocument,
+    "\n  query CatalogItems($shopIds: [ID!]!, $tagIds: [ID!]!) {\n    catalogItems(shopIds: $shopIds, tagIds: $tagIds) {\n      edges {\n        node {\n          ... on CatalogItemProduct {\n            product {\n              title\n              description\n              _id\n              variants {\n                _id\n                title\n                media {\n                  URLs {\n                    small\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n": types.CatalogItemsDocument,
+    "\n    query GetProducts($tagIds: [ID!]) {\n      tags(shopId: \"cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==\") {\n        nodes {\n          _id\n          name\n          displayTitle\n          slug\n        }\n      }\n      catalogItems(\n        shopIds: [\"cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==\"]\n        tagIds: $tagIds\n      ) {\n        edges {\n          node {\n            ... on CatalogItemProduct {\n              product {\n                title\n                description\n                pricing {\n                  displayPrice\n                }\n                _id\n                variants {\n                  _id\n                  title\n                  media {\n                    URLs {\n                      small\n                    }\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  ": types.GetProductsDocument,
+};
+
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  *
@@ -16,6 +31,19 @@ const documents = [];
  * Please regenerate the types.
  */
 export function gql(source: string): unknown;
+
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  query catalogItemProduct($shopId: ID!, $slugOrId: String!) {\n    catalogItemProduct(shopId: $shopId, slugOrId: $slugOrId) {\n      product {\n        title\n        description\n        variants {\n          _id\n          title\n          attributeLabel\n          optionTitle\n          pricing {\n            displayPrice\n            price\n            currency {\n              code\n            }\n          }\n        }\n        primaryImage {\n          URLs {\n            medium\n            original\n          }\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query catalogItemProduct($shopId: ID!, $slugOrId: String!) {\n    catalogItemProduct(shopId: $shopId, slugOrId: $slugOrId) {\n      product {\n        title\n        description\n        variants {\n          _id\n          title\n          attributeLabel\n          optionTitle\n          pricing {\n            displayPrice\n            price\n            currency {\n              code\n            }\n          }\n        }\n        primaryImage {\n          URLs {\n            medium\n            original\n          }\n        }\n      }\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  query CatalogItems($shopIds: [ID!]!, $tagIds: [ID!]!) {\n    catalogItems(shopIds: $shopIds, tagIds: $tagIds) {\n      edges {\n        node {\n          ... on CatalogItemProduct {\n            product {\n              title\n              description\n              _id\n              variants {\n                _id\n                title\n                media {\n                  URLs {\n                    small\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query CatalogItems($shopIds: [ID!]!, $tagIds: [ID!]!) {\n    catalogItems(shopIds: $shopIds, tagIds: $tagIds) {\n      edges {\n        node {\n          ... on CatalogItemProduct {\n            product {\n              title\n              description\n              _id\n              variants {\n                _id\n                title\n                media {\n                  URLs {\n                    small\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n    query GetProducts($tagIds: [ID!]) {\n      tags(shopId: \"cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==\") {\n        nodes {\n          _id\n          name\n          displayTitle\n          slug\n        }\n      }\n      catalogItems(\n        shopIds: [\"cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==\"]\n        tagIds: $tagIds\n      ) {\n        edges {\n          node {\n            ... on CatalogItemProduct {\n              product {\n                title\n                description\n                pricing {\n                  displayPrice\n                }\n                _id\n                variants {\n                  _id\n                  title\n                  media {\n                    URLs {\n                      small\n                    }\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  "): (typeof documents)["\n    query GetProducts($tagIds: [ID!]) {\n      tags(shopId: \"cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==\") {\n        nodes {\n          _id\n          name\n          displayTitle\n          slug\n        }\n      }\n      catalogItems(\n        shopIds: [\"cmVhY3Rpb24vc2hvcDpGN2ZrM3plR3o4anpXaWZzQQ==\"]\n        tagIds: $tagIds\n      ) {\n        edges {\n          node {\n            ... on CatalogItemProduct {\n              product {\n                title\n                description\n                pricing {\n                  displayPrice\n                }\n                _id\n                variants {\n                  _id\n                  title\n                  media {\n                    URLs {\n                      small\n                    }\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  "];
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {};
