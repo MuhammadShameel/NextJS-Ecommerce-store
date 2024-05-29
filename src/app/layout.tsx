b279353dbf7fe5010c0e346cea32c820755d5ae1
MuhammadShameel <mshameelkz@gmail.com>
2024-05-29 20:35:35 +0500

    add apollo wrapper in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import { ApolloWrapper } from "./lib/apollo-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav />
-        {children}
-        <Footer />
+        <ApolloWrapper>
+          <Nav />
+          {children}
+          <Footer />
+        </ApolloWrapper>
       </body>
     </html>
   );
