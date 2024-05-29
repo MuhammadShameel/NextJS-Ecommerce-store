4f839fff612bca6a620296c3d649593743953c05
MuhammadShameel <mshameelkz@gmail.com>
2024-05-29 20:32:30 +0500

    edit all product page

--- src/app/components/AllProducts.tsx
+++ src/app/components/AllProducts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
-import food from "../../../public/images/food-card.jpg";
+import food from "../../../public/images/burger.jpg";
 import Image from "next/image";
 import Link from "next/link";
 
