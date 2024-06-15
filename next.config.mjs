9db7524814767c925b2137964623ad6ea2d0f93a
MuhammadShameel <mshameelkz@gmail.com>
2024-06-15 16:37:31 +0500

    add path for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "explore-btk-opencommerce-apis.ceultnteo3kpk.ap-southeast-1.cs.amazonlightsail.com",
+        port: "",
+        pathname: "/assets/files/Media/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
