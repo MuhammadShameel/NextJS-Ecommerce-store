8f48a7f3d8ccdcd5a038e41b82de8c4a03c80bf0
MuhammadShameel <mshameelkz@gmail.com>
2024-06-28 21:00:41 +0500

    create card component and use props

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,47 +1,86 @@
+// import React from "react";
+// import Link from "next/link";
+// import Image from "next/image";
+// import placeholderimg from "../../../public/images/elementor-placeholder-image.webp";
+// import CardLoader from "../skeletonLoading/cardLoader";
+
+// const Card = ({ productsData }: any) => {
+//   if (!productsData) return <CardLoader />;
+
+//   return (
+//     <div className="container mx-auto font-open-sans">
+//       <div className="product-section p-6">
+//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+//           {productsData?.catalogItems?.edges?.map((edge: any) => {
+//             const { product } = edge.node;
+//             return (
+//               <div
+//                 key={product._id}
+//                 className="product-card blur-bg relative flex flex-col overflow-hidden bg-white shadow hover:shadow-md transition"
+//               >
+//                 <Link href={`/products/${product.slug}`}>
+//                   <Image
+//                     className="object-fill aspect-[5/4] sm:mx-auto"
+//                     src={
+//                       product?.primaryImage?.URLs?.original || placeholderimg
+//                     }
+//                     alt="product image"
+//                     width={300}
+//                     height={300}
+//                   />
+//                 </Link>
+//                 <div className="mt-4 px-3 pb-5">
+//                   <Link href={`/products/${product.slug}`}>
+//                     <h5 className="text-sm tracking-tight">{product.title}</h5>
+//                   </Link>
+//                   <div className="mt-2 flex justify-between">
+//                     <h5 className="text-sm ms-auto tracking-tight text-black">
+//                       {product.pricing.displayPrice}
+//                     </h5>
+//                   </div>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import placeholderimg from "../../../public/images/elementor-placeholder-image.webp";
-import CardLoader from "../skeletonLoading/cardLoader";
+import { CardProps } from "../types";
 
-const Card = ({ productsData }: any) => {
-  if (!productsData) return <CardLoader />;
+const Card = ({ product, imageWidth = 270, imageHeight = 150 }: CardProps) => {
+  const imageUrl = product?.primaryImage?.URLs?.original || placeholderimg;
 
   return (
-    <div className="container mx-auto font-open-sans">
-      <div className="product-section p-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {productsData?.catalogItems?.edges?.map((edge: any) => {
-            const { product } = edge.node;
-            return (
-              <div
-                key={product._id}
-                className="product-card blur-bg relative flex flex-col overflow-hidden bg-white shadow hover:shadow-md transition"
-              >
-                <Link href={`/products/${product.slug}`}>
-                  <Image
-                    className="object-fill aspect-[5/4] sm:mx-auto"
-                    src={
-                      product?.primaryImage?.URLs?.original || placeholderimg
-                    }
-                    alt="product image"
-                    width={300}
-                    height={300}
-                  />
-                </Link>
-                <div className="mt-4 px-3 pb-5">
-                  <Link href={`/products/${product.slug}`}>
-                    <h5 className="text-sm tracking-tight">{product.title}</h5>
-                  </Link>
-                  <div className="mt-2 flex justify-between">
-                    <h5 className="text-sm ms-auto tracking-tight text-black">
-                      {product.pricing.displayPrice}
-                    </h5>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+    <div className="relative m-5 flex w-full max-w-[270px] flex-col overflow-hidden bg-white hover:shadow-md">
+      <Link
+        className="relative flex overflow-hidden"
+        href={`/products/${product?.slug}`}
+      >
+        <Image
+          className="object-cover"
+          src={imageUrl}
+          alt="product image"
+          width={imageWidth}
+          height={imageHeight}
+        />
+      </Link>
+      <div className="mt-4 px-3 pb-5">
+        <h5 className="text-sm tracking-tight font-bold font-sans">
+          {product.title}
+        </h5>
+        <div className="mt-2">
+          <h5 className="text-sm tracking-tight text-right font-sans ml-auto">
+            {product?.pricing[0]?.displayPrice}
+          </h5>
         </div>
       </div>
     </div>
