a262b00525df38072fdd5ecb3697757bc9a14743
MuhammadShameel <mshameelkz@gmail.com>
2024-08-05 19:32:03 +0500

    create banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,9 +107,9 @@ export default function Home() {
                   scroll={false}
                   className={`${
                     !slug || slug === "all-products"
-                      ? " text-black bg-[#e9a70b]  border-[#e9a70b] border rounded-full p-2"
-                      : "text-[#e9a70b]"
-                  }   bg-transparent border-[#e9a70b] border rounded-full p-2 transition-colors duration-300 ease-in-out`}
+                      ? " text-black bg-[#e9a70b] opacity-100  border-[#e9a70b] border rounded-full p-2"
+                      : "border-[#272727] "
+                  } text-[#272727] opacity-[0.3]  bg-transparent border rounded-full p-2 transition-colors duration-300 ease-in-out`}
                 >
                   All Products
                 </Link>
@@ -156,6 +156,39 @@ export default function Home() {
           )}
         </div>
       </div>
+      <section className="bg-[url('../../public/images/back-30.jpg')] border-none bg-no-repeat bg-center">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+          <h1 className=" text-4xl capitalize font-bold mb-4  tracking-tight leading-none text-white">
+            15 MINUTES
+          </h1>
+          <p className="mb-8 text-xl font-bold text-white lg:text-xl sm:px-16 lg:px-48">
+            You Order We Deliver
+          </p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a
+              href="#"
+              className="inline-flex capitalize rounded-full justify-center items-center py-3 px-5 text-base font-medium text-center text-white  border border-white hover:bg-black hover:text-white hover:border-black transition-colors ease-in-out delay-75"
+            >
+              Make an order
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
       {/* Conditionally render the notification */}
       {showNotification && <Notification message="Item added to cart!" />}
     </div>
