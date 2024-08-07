c38d1f98cdf5a3bd30a18babcbfa742487622a36
MuhammadShameel <mshameelkz@gmail.com>
2024-08-07 20:43:07 +0500

    favorite icons remove

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,33 +3,15 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { MdFavorite } from "react-icons/md";
-import { useFavorites } from "@/app/context/FavoriteContext";
+
 import placeholderimg from "../../../public/images/dummy-img-white.png";
 import { CardProps } from "@/app/types";
 
-const Card = ({ title, imageUrl, href, price, product }: CardProps) => {
-  const { favorites, addToFavorites, removeFromFavorites } = useFavorites();
-  const isFavorite = favorites.some((fav) => fav._id === product._id);
-
-  const toggleFavorite = () => {
-    if (isFavorite) {
-      removeFromFavorites(product._id);
-    } else {
-      addToFavorites(product);
-    }
-  };
-
+const Card = ({ title, imageUrl, href, price }: CardProps) => {
   const imageSrc = imageUrl || placeholderimg;
 
   return (
     <div className="relative m-5 flex w-full max-w-[270px] flex-col overflow-hidden rounded-3xl bg-white transition-transform ease-in-out hover:shadow-md">
-      <MdFavorite
-        className={`relative left-56 my-2 text-2xl cursor-pointer ${
-          isFavorite ? "text-[#e9a70b]" : "text-[#f4f1e9]"
-        } transition-colors duration-300 ease-in-out`}
-        onClick={toggleFavorite}
-      />
       <Link className="relative flex overflow-hidden" href={href}>
         <Image
           className="object-cover"
@@ -48,6 +30,7 @@ const Card = ({ title, imageUrl, href, price, product }: CardProps) => {
             {price}
           </h5>
         </div>
+
         <div className="mt-2 flex justify-center">
           <button className="border border-black py-2 px-4 hover:bg-[#e9a70b] hover:border-[#e9a70b] rounded-full transition-colors duration-300 ease-in-out">
             Add to cart
