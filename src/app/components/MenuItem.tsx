b692d08328e403fd08d3b077015774607e92f17b
MuhammadShameel <mshameelkz@gmail.com>
2024-08-07 20:43:18 +0500

    add some css

--- src/app/components/MenuItem.tsx
+++ src/app/components/MenuItem.tsx
@@ -9,11 +9,11 @@ import "@/app/style/global.scss";
 const MenuItem = ({ text, slug, isActive, variant }: MenuItemProps) => {
   return (
     <div className="mx-auto">
-      <li className={`mr-5`}>
+      <li className={`mr-5 capitalize`}>
         <Link
           href={`/?tag=${slug}`}
           scroll={false}
-          className={`hover:text-black hover:bg-[#e9a70b] hover:opacity-100 hover:border-[#e9a70b] transition-colors duration-300 ease-in-out ${
+          className={`hover:text-black capitalize hover:bg-[#e9a70b] hover:opacity-100 hover:border-[#e9a70b] transition-colors duration-300 ease-in-out ${
             isActive
               ? "  text-black bg-[#e9a70b]  border-[#e9a70b] border rounded-full p-2  "
               : "text-[#272727] bg-transparent  border-[#272727] opacity-[0.3] border rounded-full p-2"
